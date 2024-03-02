2656d86f8ee1fc7e0e54daf5941a4c046724fbfa
P.ARUN <95969896+acs-web-tech@users.noreply.github.com>
2024-03-03 03:41:29 +0530

    Delete querystr.js

--- querystr.js
+++
@@ -1,8 +0,0 @@
-const ADMIN_CREATE = `
-INSERT INTO admin_create
- (id, admin_id, profile_pic, user_name, password, company_name, com_logo, designation, email_id, mobile_no, doc_upload, subs_from, subs_to, status, role_id, role_name, createdtime, createdby, short_name)
-  VALUES
-(?, ?, ?, ?, ?, ?, ?,?,?,?,?,?,?,?,?,?,?,?,?);
-
-  `
-module.exports.ADMIN_CREATE = ADMIN_CREATE
